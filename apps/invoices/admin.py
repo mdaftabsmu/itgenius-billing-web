a19846809986f3a4dd4b3fddf4b17565072e8ab8
MD Aftab Alam <mdaftabalam196@gmail.com>
2026-08-08 22:23:16 +0530

    feat: register invoices in admin

---
+++ apps/invoices/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Invoice, InvoiceItem
+
+class InvoiceItemInline(admin.TabularInline):
+    model = InvoiceItem
+    extra = 0
+
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ("invoice_number", "customer", "invoice_date", "grand_total", "paid_amount", "status")
+    search_fields = ("invoice_number", "customer__name", "customer__company_name")
+    list_filter = ("status", "invoice_date")
+    inlines = [InvoiceItemInline]
