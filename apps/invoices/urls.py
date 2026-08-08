f7c3cd9866a5e5dee9fdf1c768db31bc19002fd3
MD Aftab Alam <mdaftabalam196@gmail.com>
2026-08-08 22:23:09 +0530

    feat: add invoice URL routes

---
+++ apps/invoices/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import invoice_create, invoice_detail, invoice_list
+
+urlpatterns = [
+    path("", invoice_list, name="invoice_list"),
+    path("new/", invoice_create, name="invoice_create"),
+    path("<int:pk>/", invoice_detail, name="invoice_detail"),
+]
