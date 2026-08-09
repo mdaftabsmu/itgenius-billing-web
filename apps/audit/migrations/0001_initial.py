0a64eb74242bf3e53fe65690bb7d47b7f1571fc3
MD Aftab Alam <mdaftabalam196@gmail.com>
2026-08-09 09:43:16 +0530

    feat: add audit log initial migration

---
+++ apps/audit/migrations/0001_initial.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
+
+class Migration(migrations.Migration):
+    initial = True
+    dependencies = [(migrations.swappable_dependency(settings.AUTH_USER_MODEL))]
+    operations = [migrations.CreateModel(name="AuditLog", fields=[
+        ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+        ("action", models.CharField(max_length=30)), ("model_name", models.CharField(blank=True, max_length=100)),
+        ("object_id", models.CharField(blank=True, max_length=100)), ("path", models.CharField(blank=True, max_length=500)),
+        ("ip_address", models.GenericIPAddressField(blank=True, null=True)), ("details", models.JSONField(blank=True, default=dict)),
+        ("created_at", models.DateTimeField(auto_now_add=True)),
+        ("user", models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL)),
+    ], options={"ordering": ["-created_at"]})]
